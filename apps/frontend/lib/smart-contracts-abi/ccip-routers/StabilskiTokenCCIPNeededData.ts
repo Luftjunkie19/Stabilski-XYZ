57acde3ecc3a91bc4ab617648e171deb7540c6d0
Luftjunkie19 <luftjunkie@protonmail.com>
2025-11-15 07:47:07 +0100

    Deploy InformationCCIPRetriever contracts on multiple chains with transaction details and receipts

--- apps/frontend/lib/smart-contracts-abi/ccip-routers/StabilskiTokenCCIPNeededData.ts
+++ apps/frontend/lib/smart-contracts-abi/ccip-routers/StabilskiTokenCCIPNeededData.ts
@@ -9,10 +9,10 @@ export const stabilskiTokenPoolEthSepolia = "0x98C7955FaBB14d83D874B516f4C849f4E
 export const stabilskiTokenPoolArbSepolia= "0xb0Cb10e3872A9F955758893E135A0C3579EA0187";
 
 
-export const ccipInformationRetrieverSepoliaEthAddress = "0x9Abcf91e1E301091EC5cc808e1ab7916a4c032A1";
+export const ccipInformationRetrieverSepoliaEthAddress = "0xDEd9cf2f291e0eC1f17fB73b7a8D52E606b90900";
 
-export const ccipInformationRetrieverSepoliaArbAddress = "0x12A6a3d30Fa3327338a4faE0f6ec0BDcf52eE715";
+export const ccipInformationRetrieverSepoliaArbAddress = "0x6057EA55A5161EFAEd5aDa8a2D2006fD9c9A8902";
 
-export const ccipInformationRetrieverSepoliaBaseAddress= "0xfC972770fc1b35A0F4eEb26bA56e6D294469308e";
+export const ccipInformationRetrieverSepoliaBaseAddress= "0xc912a30a09590fF9cd9FE424db744f208dE47A39";
 
 export const ccipInformationRetrieverAbi= [{"inputs":[{"internalType":"address","name":"stabilskiSourceAddress","type":"address"},{"internalType":"address","name":"routerAddress","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"receiverAddress","type":"address"},{"internalType":"uint256","name":"amountToSend","type":"uint256"},{"internalType":"uint64","name":"destinationSelector","type":"uint64"}],"name":"getCCIPMessageFee","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"receiverAddress","type":"address"},{"internalType":"uint256","name":"amountToSend","type":"uint256"}],"name":"getCcipMessage","outputs":[{"components":[{"internalType":"bytes","name":"receiver","type":"bytes"},{"internalType":"bytes","name":"data","type":"bytes"},{"components":[{"internalType":"address","name":"token","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"internalType":"struct Client.EVMTokenAmount[]","name":"tokenAmounts","type":"tuple[]"},{"internalType":"address","name":"feeToken","type":"address"},{"internalType":"bytes","name":"extraArgs","type":"bytes"}],"internalType":"struct Client.EVM2AnyMessage","name":"","type":"tuple"}],"stateMutability":"view","type":"function"}];
